ec394b68e25d80174c81a92a4b5054e848b00f26
epistemik <epistemik@gmail.com>
2018-12-08 08:37:37 -0500

    pass filename as parameter to main

--- parsePdf/tryPyPDF2.py
+++ parsePdf/tryPyPDF2.py
@@ -1,19 +1,54 @@
 #!/usr/bin/env python
 
+# tryPyPDF2.py -- parse a PDF file and recover the text
+#
+# Copyright (c) 2018, 2019 Mark Sattolo <epistemik@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# @author Mark Sattolo <epistemik@gmail.com>
+
+__updated__ = "2018-12-08 06:25"
+
 import sys
 import collections
 import json
 import PyPDF2
 
-monarch = "/home/marksa/Downloads/ClientTransactionsReport.pdf"
+def getPage(readPdf, pageNum):
+    page = readPdf.getPage(pageNum)
+    page_content = page.extractText()
+    print("content of page #{0}:".format(sys.argv[1]))
+    print page_content.encode('utf-8')
+    
+def getAllPages(readPdf):
+    c = collections.Counter(range(readPdf.getNumPages()))
+    for i in c:
+       page = readPdf.getPage(i)
+       page_content = page.extractText()
+       print page_content.encode('utf-8')
 
 def main():
     print("len(sys.argv) = {0}".format(len(sys.argv)))
+    if len(sys.argv) < 2:
+        print("Usage: python {} <reportPath> [pageNum]".format(sys.argv[0]))
+        
+    monarch = sys.argv[1]
+    print("Monarch report is: {}".format(monarch))
+    
     pageNum = 0
     readAll = True
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
         pageNum = int(sys.argv[1]) - 1
-        readAll = True
+        readAll = False
         
     pdfFile = open(monarch, 'rb')
     pdfReader = PyPDF2.PdfFileReader(pdfFile)
@@ -29,18 +64,5 @@ def main():
         
     print("Bye!\n")
     
-def getPage(readPdf, pageNum):
-    page = readPdf.getPage(pageNum)
-    page_content = page.extractText()
-    print("content of page #{0}:".format(sys.argv[1]))
-    print page_content.encode('utf-8')
-    
-def getAllPages(readPdf):
-    c = collections.Counter(range(readPdf.getNumPages()))
-    for i in c:
-       page = readPdf.getPage(i)
-       page_content = page.extractText()
-       print page_content.encode('utf-8')
-
 if __name__ == "__main__":
     main()
