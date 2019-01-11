ede6d39ea5c62c42c9d8683623c51467c750c645
epistemik <epistemik@gmail.com>
2019-01-11 12:36:32 -0500

    use print_info() and print_error()

--- makeGncTx/main.py
+++ makeGncTx/main.py
@@ -22,24 +22,25 @@ __updated__ = "2019-01-01 09:11"
 import os.path as osp
 from sys import argv, exit
 from parseMonarch import parse_monarch_report
-from createGnucashTxs import create_gnc_txs, PRAC1_GNC
+from createGnucashTxs import create_gnc_txs
+from Configuration import *
 
 
 def main():
     if len(argv) < 4:
-        print("NOT ENOUGH parameters!")
-        print("usage: python {0} <monarch file> <gnucash file> <mode: prod|test>".format(argv[0]))
-        print("Example: {0} '{1}' '{2}' 'test'".format(argv[0], "txtFromPdf/Monarch-Mark-all.txt", PRAC1_GNC))
+        print_error("NOT ENOUGH parameters!")
+        print_info("usage: python {0} <monarch file> <gnucash file> <mode: prod|test>".format(argv[0]), color=YELLOW)
+        print_info("Example: {0} '{1}' '{2}' 'test'".format(argv[0], "txtFromPdf/Monarch-Mark-all.txt", PRAC1_GNC), color=CYAN)
         exit(4)
 
     mon_file = argv[1]
     if not osp.isfile(mon_file):
-        print("File path '{}' does not exist. Exiting...".format(mon_file))
+        print_error("File path '{}' does not exist. Exiting...".format(mon_file))
         exit(1)
 
     gnc_file = argv[2]
     if not osp.isfile(gnc_file):
-        print("File path '{}' does not exist. Exiting...".format(gnc_file))
+        print_error("File path '{}' does not exist. Exiting...".format(gnc_file))
         exit(2)
 
     mode = argv[3]
@@ -50,7 +51,7 @@ def main():
     # create gnucash transactions and write to the desired Gnucash file
     create_gnc_txs(record, gnc_file, mode)
 
-    print("\n >>> PROGRAM ENDED.")
+    print_info("\n >>> PROGRAM ENDED.", color=GREEN)
 
 
 if __name__ == '__main__':
