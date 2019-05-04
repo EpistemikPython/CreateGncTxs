9023c0d0682be6ea2c2bf09db9d90273937219a1
epistemik <epistemik@gmail.com>
2019-05-04 16:33:23 -0400

    convert print() to print_info()

--- parsePdf/parsePdf.py
+++ parsePdf/parsePdf.py
@@ -4,28 +4,27 @@
 # Copyright (c) 2018,2019 Mark Sattolo <epistemik@gmail.com>
 #
 # @author Mark Sattolo <epistemik@gmail.com>
-# @revised 2019-03-02
 # @version Python 3.6
-
-
-__updated__ = "2019-03-16"
+# @created 2018-12
+# @updated 2019-04-28
 
 from sys import argv, exit
 import os.path as osp
 import collections
 import json
-import datetime as dt
+from datetime import datetime as dt
 import PyPDF2
+from Configuration import *
 
-now = dt.datetime.strftime(dt.datetime.now(), "%Y-%m-%dT%H-%M-%S")
+now = dt.now().strftime("%Y-%m-%dT%H-%M-%S")
 
 
 # noinspection PyPep8
 def get_page(read_pdf, page_num):
     page = read_pdf.getPage(page_num)
     page_content = page.extractText()
-    print("content of page #{}:".format(page_num+1))
-    print( page_content.encode('utf-8') )
+    print_info("content of page #{}:".format(page_num+1), BLUE)
+    print_info(page_content.encode('utf-8'), GREEN)
 
 
 def get_all_pages(pdf_reader, fp):
@@ -36,19 +35,19 @@ def get_all_pages(pdf_reader, fp):
         # fix Python 2 to Python 3
         # CANNOT change page text to utf-8 with Python 3 or lose newlines...
         # page_content = page_text.encode('utf-8')
-        print(page_text)
+        print_info(page_text, BLUE)
         fp.write(page_text)
 
 
 def parse_pdf_main():
     exe = argv[0].split('/')[-1]
-    print("len(argv) = {}".format(len(argv)))
+    print_info("len(argv) = {}".format(len(argv)))
     if len(argv) < 2:
-        print("Usage: python {} <pdf_input_path> [page_num]".format(exe))
+        print_error("Usage: python {} <pdf_input_path> [page_num]".format(exe))
         exit()
 
     monarch = argv[1]
-    print("Monarch report is: {}".format(monarch))
+    print_info("Monarch report is: {}".format(monarch), MAGENTA)
 
     page_num = 0
     read_all = True
@@ -61,19 +60,20 @@ def parse_pdf_main():
     pdf_reader = PyPDF2.PdfFileReader(pdf_file)
 
     doc_info = pdf_reader.getDocumentInfo()
-    print("doc_info = {0}".format(json.dumps(doc_info)))
-    print("number of pages = {0}".format(pdf_reader.getNumPages()))
+    print_info("doc_info = {0}".format(json.dumps(doc_info)), BLUE)
+    print_info("number of pages = {0}".format(pdf_reader.getNumPages()), CYAN)
 
     # print info as txt file
     # pluck path and basename from pdf file name to use for the saved file
     (path, fname) = osp.split(monarch)
-    # print("path is '{}'".format(path))
+    # print_info("path is '{}'".format(path))
+
     # save to the output folder
     path = path.replace('in', 'out')
     (basename, ext) = osp.splitext(fname)
     # add a timestamp to get a unique file name
     out_file = path + '/' + basename + '.' + now + '.txt'
-    print("out_file is '{}'".format(out_file))
+    print_info("out_file is '{}'".format(out_file), GREEN)
     fp = open(out_file, 'w')
 
     if read_all:
@@ -83,7 +83,7 @@ def parse_pdf_main():
 
     fp.close()
 
-    print("\n >>> PROGRAM ENDED.")
+    print_info("\n >>> PROGRAM ENDED.", CYAN)
 
 
 if __name__ == "__main__":
